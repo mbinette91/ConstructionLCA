9405e8870420e1f8a4652fb89f111dc513754b3e
Mathieu <mbinette@live.ca>
2015-05-19 01:00:15 -0400

    Integrated D3JS (Issue #2) with garbage data. Tried to fix bugs with resizing (and going in fullscreen mode). Very buggy. Lots of work to do.

--- webapp/js/GUI.js
+++ webapp/js/GUI.js
@@ -8,8 +8,20 @@ GUI.prototype.initialize = function() {
     this.initializeTabs();
     this.initializeLayout();
     this.initializeFullScreenManager();
+    this.initializeStats();
     this.initializeUnity();
+
+    var that = this;
+    $(window).resize( function(){ that.handleWindowSizeChanged(); } );
 };
+
+
+GUI.prototype.handleWindowSizeChanged = function() {
+    var that = this;
+    setTimeout(function(){
+        that.refreshGraph();
+    }, 250); // Artificial timeout because the layout plugin needs to update itself.
+}
 
 GUI.prototype.initializeTabs = function() {
     $('#tabs a').click(function(e) {
@@ -44,7 +56,77 @@ GUI.prototype.initializeStats = function(){
             }, 2000);
         }
         $('.progress-bar').css('width', statsPercentLoaded+'%').attr('aria-valuenow', statsPercentLoaded); 
-    }, 100);
+    }, 10);
+
+    this.initializeD3JS();
+}
+
+GUI.prototype.initializeD3JS = function(){
+    var width = $("#section-container").width(),
+        height = $("#section-container").height();
+
+    var that = this
+    this.xRange = d3.scale.linear()
+        .range([0, width]);
+
+    this.yRange = d3.scale.linear()
+        .range([0, height]);
+
+    var color = d3.scale.category20c();
+
+    var partition = d3.layout.partition()
+        .children(function(d) { return isNaN(d.value) ? d3.entries(d.value) : null; })
+        .value(function(d) { return d.value; });
+
+    var svg = d3.select("#Stats").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+    that.rect = svg.selectAll("rect");
+
+
+    this.clicked = function (d) {
+      that.xRange.domain([d.x, d.x + d.dx]);
+      that.yRange.domain([d.y, 1]).range([d.y ? 20 : 0, height]);
+
+      that.rect.transition()
+          .duration(750)
+          .attr("x", function(d) { return that.xRange(d.x); })
+          .attr("y", function(d) { return that.yRange(d.y); })
+          .attr("width", function(d) { return that.xRange(d.x + d.dx) - that.xRange(d.x); })
+          .attr("height", function(d) { return that.yRange(d.y + d.dy) - that.yRange(d.y); });
+    }
+
+    d3.json("data.json", function(error, root) {
+      that.rect = that.rect
+          .data(partition(d3.entries(root)[0]))
+        .enter().append("rect")
+          .attr("x", function(d) { return that.xRange(d.x); })
+          .attr("y", function(d) { return that.yRange(d.y); })
+          .attr("width", function(d) { return $(window).width(); })
+          .attr("height", function(d) { return  $(window).height(); })
+          .attr("fill", function(d) { return color((d.children ? d : d.parent).key); })
+          .on("click", that.clicked);
+    });
+}
+
+GUI.prototype.refreshGraph = function(){
+    var width = $("#section-container").width(),
+        height = $("#section-container").height();
+
+    this.xRange.range([0, width]);
+    this.yRange.range([0, height]);
+
+    var svg = $("#Stats svg")
+        .attr("width", width)
+        .attr("height", height);
+
+      that.rect.transition()
+          .attr("x", function(d) { return that.xRange(d.x); })
+          .attr("y", function(d) { return that.yRange(d.y); })
+          .attr("width", function(d) { return that.xRange(d.x + d.dx) - that.xRange(d.x); })
+          .attr("height", function(d) { return that.yRange(d.y + d.dy) - that.yRange(d.y); });
 }
 
 GUI.prototype.initializeUnity = function(){
