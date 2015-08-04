c3f0e5bb976ee51bca9f5b0da078e61cc90f9acb
Mathieu <mbinette@live.ca>
2015-08-04 06:54:09 -0400

    Amélioration de l’apparence de la page de création de projet

--- webapp/js/utils.js
+++ webapp/js/utils.js
@@ -4,7 +4,7 @@ function rgb(r, g, b){
 }
 
 $.fn.exists = function(a){
-    return this.length;
+  return this.length;
 };
 
 $.fn.equals = function(compareTo) {
@@ -18,3 +18,11 @@ $.fn.equals = function(compareTo) {
   }
   return true;
 };
+
+/*File input JS, source: http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3/*/
+$(document).on('change', '.btn-file :file', function() {
+  var input = $(this),
+    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+});
